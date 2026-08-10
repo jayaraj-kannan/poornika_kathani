19e716fd22353503a35a3de694f614188aa25c9e
Jayaraj Kannan <jayarajkannan95@gmail.com>
2026-08-10 23:42:43 +0530

    layout fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const customFont = localFont({
 
 
 export const metadata: Metadata = {
-  title: "காாதணி விழா அழைப்பிதழ் | ஜ வி பூர்ணிகா",
-  description: "திரு க ஜெயராஜ் & திருமதி விஷாலி அவர்களின் அன்பு மகள் ஜ வி பூர்ணிகாவின் காது குத்தும் விழா அழைப்பிதழ்",
+  title: "காாதணி விழா அழைப்பிதழ் | ஜெ.வி பூர்ணிகா",
+  description: "திரு ஜெயராஜ் & திருமதி விஷாலி அவர்களின் அன்பு மகள் ஜெ.வி பூர்ணிகாவின் காது குத்தும் விழா அழைப்பிதழ்",
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">👼🏻</text></svg>',
   },
