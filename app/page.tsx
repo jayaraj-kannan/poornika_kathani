8ff8165e575296b161615a67e480818b23b3d6d2
Jayaraj Kannan <jayarajkannan95@gmail.com>
2026-08-15 10:16:09 +0530

    updated maps

--- app/page.tsx
+++ app/page.tsx
@@ -518,7 +518,7 @@ export default function EarPiercingInvitation() {
 
 
   const mapsUrl =
-    "https://maps.app.goo.gl/ZjvbQTxL5djhuXxR9";
+    "https://www.google.com/maps/search/?api=1&query=Shree+Periya+Nayagi+,+Periyandar+Kovil&query_place_id=ChIJKSmeiymgVDoFrDCO0AGtNps";
 
   return (
     <div className="relative min-h-screen w-full flex items-center justify-center bg-black text-amber-50 selection:bg-amber-500 selection:text-black font-sans py-2 sm:py-6">
